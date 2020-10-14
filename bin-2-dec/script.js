d38a66d8df4ca9cbc3721252c8ef140293d35aac
Victor <victorzevallos@protonmail.com>
2020-10-14 12:30:33 -0500

    First commit

---
+++ bin-2-dec/script.js
@@ -0,0 +1,18 @@
+const binary = document.querySelector("#binary")
+    const decimal = document.querySelector("#decimal")
+    const notification = document.querySelector("#notification")
+
+    binary.addEventListener("input", e => {
+      if (isBinary(binary.value)) {
+        notification.textContent = ""
+        decimal.value = parseInt(binary.value, 2)
+      } else {
+        decimal.value = ""
+        notification.textContent = "El número debe ser binario"
+      }
+    })
+
+    function isBinary(text) {
+      if (text == "") return false
+      return text.split("").every(digit => digit == "1" || digit == "0")
+    }
