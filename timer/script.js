6c31cd83126016cec07704916cddf69abb1fd68a
Victor <victorzevallos@protonmail.com>
2020-10-21 10:03:41 -0500

    Update timer

--- timer/script.js
+++ timer/script.js
@@ -1,30 +1,58 @@
-const hours = +document.querySelector("#hours").value
-const minutes = +document.querySelector("#minutes").value
-const seconds = +document.querySelector("#seconds").value
+const hours = document.querySelector("#hours")
+const minutes = document.querySelector("#minutes")
+const seconds = document.querySelector("#seconds")
 const timer = document.querySelector("#timer")
 const start = document.querySelector("#start")
+const reset = document.querySelector("#reset")
+const pause = document.querySelector("#pause")
+let startInterval
+let totalSeconds = inputToSeconds()
 
-// TODO: Corregir los segundos
-start.addEventListener("click", e => {
-  let totalSeconds = hours * 3600 + minutes * 60 + seconds
+start.addEventListener("click", () => {
+  reset.disabled = true
+  start.style.zIndex = "0"
+  pause.style.zIndex = "10"
+
 
   timer.textContent = formatTimer(secondsToTimer(totalSeconds))
-  console.log(secondsToTimer(totalSeconds))
-  setInterval(() => {
-    totalSeconds--
-    timer.textContent = formatTimer(secondsToTimer(totalSeconds))
+
+  startInterval = setInterval(() => {
+    timer.textContent = formatTimer(secondsToTimer(--totalSeconds))
+    if (totalSeconds === 0) {
+      document.getElementById("tictac").play()
+      clearInterval(startInterval)
+    }
   }, 1000);
 })
+
+pause.addEventListener("click", () => {
+  start.style.zIndex = "10"
+  pause.style.zIndex = "0"
+  reset.disabled = false
+  clearInterval(startInterval)
+})
+
+
+reset.addEventListener("click", () => {
+  totalSeconds = inputToSeconds()
+  timer.textContent = formatTimer(secondsToTimer(totalSeconds))
+})
+
+function inputToSeconds() {
+  return parseInt(hours.value) * 3600 + parseInt(minutes.value) * 60 + parseInt(seconds.value)
+}
 
 function secondsToTimer(seconds) {
   const hours = Math.floor(seconds / 3600)
   seconds -= hours * 3600
-  const minutes = Math.floor( seconds / 60)
+  const minutes = Math.floor(seconds / 60)
   seconds -= minutes * 60
   return {hours, minutes, seconds}
 }
 
-// Todo: El formato debe ser hh:mm:ss
 function formatTimer({hours, minutes, seconds}) {
+  hours = hours.toString().padStart(2, "0")
+  minutes = minutes.toString().padStart(2, "0")
+  seconds = seconds.toString().padStart(2, "0")
   return `${hours}:${minutes}:${seconds}`
 }
