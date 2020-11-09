e6b1484d6e1d5bbcd62e292ebd343f1f77bc6a80
Victor <victorzevallos@protonmail.com>
2020-11-09 14:16:34 -0500

    Change phrases

--- typing/phrases.js
+++ typing/phrases.js
@@ -1,58 +1,102 @@
 const phrases = [
   {
-    text: "Un lenguaje de programación es de bajo nivel cuando requiere que prestes atención a lo irrelevante.",
-    by: "Alan J. Perlis"
-  },
-  {
-    text: "La educación en computación no puede hacer a nadie un experto programador más que el estudio de pinceles y pigmentos puede hacer a alguien un pintor experto.",
-    by: "Eric S. Raymond"
-  },
-  {
-    text: "Teniendo en cuenta el estado actual triste de nuestros programas de computadora, el desarrollo de software es todavía un arte negro, y aún no pueden ser llamado una disciplina de ingeniería.",
-    by: "Bill Clinton"
-  },
-  {
-    text: "El uso de Cobol paraliza la mente, su enseñanza, por tanto, debe ser considerada como un delito penal.",
-    by: "E.W. Dijkstra"
-  },
-  {
-    text: "Perfección en el diseño se alcanza, no cuando no hay nada más que añadir, sino cuando no queda nada para quitar.",
-    by: "Antoine de Saint-Exupéry"
-  },
-  {
-    text: "Los mejores programadores no son marginalmente mejores que los simplemente buenos. Ellos son un orden de magnitud mayor, medida por cualquier estándar: creatividad conceptual, velocidad, ingenio de diseño, o la capacidad de resolución de problemas.",
-    by: "Randall E. Stross"
-  },
-  {
-    text: "Medir los avances de programación por líneas de código es como medir el progreso de la construcción de aviones por peso.",
-    by: "Bill Gates"
-  },
-  {
-    text: "La depuración es dos veces más duro que escribir el código. Por lo tanto, si usted escribe código de la forma más inteligente posible, por definición, no es lo suficientemente inteligente para depurarlo.",
-    by: "Brian W. Kernighan"
-  },
-  {
-    text: "La gente piensa que la informática es el arte de genios, pero la verdadera realidad es lo contrario, sólo son mucha gente haciendo cosas que se acumulan unos sobre otros, como un muro de piedras.",
-    by: "Donald Knuth"
-  },
-  {
-    text: "Primero aprenda informática y toda la teoría. Acto seguido desarrolle un estilo de programación. Luego olvidesé todo eso y solo hackee.",
-    by: "George Carrette"
-  },
-  {
-    text: "El problema de los programadores es que nunca se sabe lo que un programador está haciendo hasta que ya es demasiado tarde.",
-    by: "Seymour Cray"
-  },
-  {
-    text: "La mayoría de los buenos programadores programan, no porque esperan que se les pague o por adulación por parte del público, sino porque es divertido programar.",
-    by: "Linus Torvalds"
-  },
-  {
-    text: "Siempre programa como si la persona que finalmente mantendrá tu código fuera un psicópata violento que sabe dónde vives.",
+    text: "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
     by: "Martin Golding"
   },
   {
-    text: "Hay dos formas de construir un diseño de software. Una forma es hacerlo tan simple que es obvio que no hay deficiencias. Y la otra es hacerlo tan complicado que no hay deficiencias obvias.",
-    by: "C.A.R. Hoare"
-  }
+    text: "First, solve the problem. Then, write the code.",
+    by: "John Johnson"
+  },
+  {
+    text: "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
+    by: "Bill Gates"
+  },
+  {
+    text: "When debugging, novices insert corrective code; experts remove defective code.",
+    by: "Richard Pattis"
+  },
+  {
+    text: "Walking on water and developing software from a specification are easy if both are frozen.",
+    by: "Edward V Berard"
+  },
+  {
+    text: "We have always been shameless about stealing great ideas.",
+    by: "Steve Jobs"
+  },
+  {
+    text: "Without requirements or design, programming is the art of adding bugs to an empty \"text\" file.",
+    by: "Louis Srygley"
+  },
+  {
+    text: "It's okay to figure out murder mysteries, but you shouldn't need to figure out code. You should be able to read it.",
+    by: "Steve McConnell"
+  },
+  {
+    text: "Perfection (in design) is achieved not when there is nothing more to add, but rather when there is nothing more to take away.",
+    by: "Antoine de Saint-Exupery"
+  },
+  {
+    text: "Programming can be fun, so can cryptography; however they should not be combined.",
+    by: "Kreitzberg and Shneiderman"
+  },
+  {
+    text: "It's better to wait for a productive programmer to become available than it is to wait for the first available programmer to become productive.",
+    by: "Steve McConnell"
+  },
+  {
+    text: "Every big computing disaster has come from taking too many ideas and putting them in one place.",
+    by: "Gordon Bell"
+  },
+  {
+    text: "You have to learn the rules of the game. And then you have to play better than anyone else.",
+    by: "Albert Einstein"
+  },
+  {
+    text: "Insanity: doing the same thing over and over again and expecting different results.",
+    by: "Albert Einstein"
+  },
+  {
+    text: "Logic will get you from A to B. Imagination will take you everywhere.",
+    by: "Albert Einstein"
+  },
+  {
+    text: "When the solution is simple, God is answering.",
+    by: "Albert Einstein"
+  },
+  {
+    text: "If you can't explain it simply, you don't understand it well enough.",
+    by: "Albert Einstein"
+  },
+  {
+    text: "Talk is cheap, show me the code!",
+    by: "Linus Torvalds"
+  },
+  {
+    text: "They did not know it was impossible, so they did it!",
+    by: "Mark Twain"
+  },
+  {
+    text: "Simplicity is prerequisite for reliability.",
+    by: "Edsger W. Dijkstra"
+  },
+  {
+    text: "Focus on WHY instead of WHAT in your code will make you a better developer.",
+    by: "Jordi Boggiano"
+  },
+  {
+    text: "The best engineers I know are artists at heart. The best designers I know are secretly technicians as well.",
+    by: "Andrei Herasimchuk"
+  },
+  {
+    text: "The proper use of comments is to compensate for our failure to express ourself in code.",
+    by: "Robert C. Martin"
+  },
+  {
+    text: "Code is like humor. When you have to explain it, it's bad.",
+    by: "Cory House"
+  },
+  {
+    text: "Programmers are constantly making things more complicated than they need to be BECAUSE FUTURE. Fuck the future. Program for today.",
+    by: "David Heinemeier Hansson"
+  },
 ]
