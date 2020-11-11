55c7967dd6e83c225a927ed3102617e0fbc9e3e3
Victor <victorzevallos@protonmail.com>
2020-11-10 19:47:30 -0500

    Refactoring Cookie Clicker

--- cookie-clicker/game.js
+++ cookie-clicker/game.js
@@ -1,36 +1,37 @@
-let cookies = 0;
+const message = document.getElementById("message")
 
-const inventory = [0, 0, 0];
-const cookiesProduce = [1, 2, 4];
-const priceProduct = [100, 200, 400];
+let cookies = 1231234
+const inventory = [0, 0, 0]
+const cookiesProduce = [1, 2, 4]
+const priceProduct = [100, 200, 400]
 
 function clickToCookie() {
-  cookies++;
-  document.getElementById("message").innerHTML = "";
+  cookies++
+  message.innerHTML = ""
 }
 
-function purchase(objeto) {
-  if (cookies >= priceProduct[objeto]) {
-    inventory[objeto]++;
-    cookies -= priceProduct[objeto];
-    document.getElementById("message").innerHTML = "";
+function purchase(object) {
+  if (cookies >= priceProduct[object]) {
+    inventory[object]++
+    cookies -= priceProduct[object]
+    message.innerHTML = ""
   } else {
-    document.getElementById("message").innerHTML = "No tienes suficientes galletas";
+    massage.innerHTML = "No tienes suficientes galletas"
   }
 }
 
-const FPSProduce = 1;
+const FPSProduce = 1
 setInterval(() => {
   for (let i = 0; i < inventory.length; i++) {
-    cookies += inventory[i] * cookiesProduce[i];
+    cookies += inventory[i] * cookiesProduce[i]
   }
-}, 1000/FPSProduce);
+}, 1000/FPSProduce)
 
 
-const FPS = 30;
+const FPS = 30
 setInterval(() => {
-  document.getElementById("counter").innerHTML = cookies;
+  document.getElementById("counter").innerHTML = new Intl.NumberFormat().format(cookies)
   document.getElementById("inventory").innerHTML = `Cursores: ${inventory[0]}<br>
-                                                      Abuelitas: ${inventory[1]}<br>
-                                                      Hornos: ${inventory[2]}`;
-}, 1000/FPS);
+                                                    Abuelitas: ${inventory[1]}<br>
+                                                    Hornos: ${inventory[2]}`
+}, 1000/FPS)
