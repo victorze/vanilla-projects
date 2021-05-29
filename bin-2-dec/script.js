a832e29db6e7d41d756cc20491b9bd31725e2b53
Victor <victorze@protonmail.com>
2021-05-29 09:49:50 -0500

    Refactor bin-2-dec

--- bin-2-dec/script.js
+++ bin-2-dec/script.js
@@ -1,16 +1,16 @@
-const binary = document.querySelector("#binary")
-const decimal = document.querySelector("#decimal")
+const binary = document.querySelector("#binary");
+const decimal = document.querySelector("#decimal");
 
 binary.addEventListener("input", e => {
-  if (e.data === null) return
+  if (!e.data) return;
 
-  if (e.data !== "1" && e.data !== "0") {
-    binary.value = binary.value.slice(0, binary.value.length - 1)
+  if (!(e.data == "1" || e.data == "0")) {
+    binary.value = binary.value.slice(0, binary.value.length - 1);
   } else {
-    decimal.value = parseInt(binary.value, 2)
+    decimal.value = parseInt(binary.value, 2);
   }
-})
+});
 
 decimal.addEventListener("input", () => {
-  binary.value = parseInt(decimal.value).toString(2)
-})
+  binary.value = parseInt(decimal.value).toString(2);
+});
