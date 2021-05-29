7652bcb17d8b22f9f2c59a75899004495f2ea56c
Victor <victorze@protonmail.com>
2021-05-29 10:54:37 -0500

    Refactor cookie clicker

--- cookie-clicker/game.js
+++ cookie-clicker/game.js
@@ -1,37 +1,36 @@
-const message = document.getElementById("message")
+const message = document.getElementById("message");
 
-let cookies = 0
-const inventory = [0, 0, 0]
-const cookiesProduce = [1, 2, 4]
-const priceProduct = [100, 200, 400]
+let cookies = 0;
+const inventory = [0, 0, 0];
+const cookiesProduce = [1, 2, 4];
+const priceProduct = [100, 200, 400];
 
-function clickToCookie() {
-  cookies++
-  message.innerHTML = ""
+function addACookie() {
+  cookies++;
+  message.innerHTML = "";
 }
 
-function purchase(object) {
-  if (cookies >= priceProduct[object]) {
-    inventory[object]++
-    cookies -= priceProduct[object]
-    message.innerHTML = ""
+function purchase(cursor) {
+  if (cookies >= priceProduct[cursor]) {
+    inventory[cursor]++;
+    cookies -= priceProduct[cursor];
+    message.innerHTML = "";
   } else {
-    message.innerHTML = "No tienes suficientes galletas"
+    message.innerHTML = "No tienes suficientes galletas";
   }
 }
 
-const FPSProduce = 1
+const FPSProduce = 1;
 setInterval(() => {
   for (let i = 0; i < inventory.length; i++) {
-    cookies += inventory[i] * cookiesProduce[i]
+    cookies += inventory[i] * cookiesProduce[i];
   }
-}, 1000/FPSProduce)
+}, 1000/FPSProduce);
 
-
-const FPS = 30
+const FPS = 30;
 setInterval(() => {
-  document.getElementById("counter").innerHTML = new Intl.NumberFormat().format(cookies)
-  document.getElementById("inventory").innerHTML = `Cursores: ${inventory[0]}<br>
+  document.querySelector("#counter").innerHTML = new Intl.NumberFormat().format(cookies);
+  document.querySelector("#inventory").innerHTML = `Cursores: ${inventory[0]}<br>
                                                     Abuelitas: ${inventory[1]}<br>
-                                                    Hornos: ${inventory[2]}`
-}, 1000/FPS)
+                                                    Hornos: ${inventory[2]}`;
+}, 1000/FPS);
