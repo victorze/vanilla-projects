3343d920720a77541440391408a1c05bc7884a46
Victor <victorze@protonmail.com>
2021-05-30 12:22:20 -0500

    Refactor typing

--- typing/script.js
+++ typing/script.js
@@ -1,55 +1,58 @@
-const phraseContainer = document.querySelector(".phrase")
-const by = document.querySelector(".by")
+const phraseContainer = document.querySelector(".phrase");
+const by = document.querySelector(".by");
 
-const phrase = phrases[Math.floor(Math.random() * phrases.length)]
+const phrase = phrases[Math.floor(Math.random() * phrases.length)];
 
-phraseContainer.innerHTML = phrase.text.split("").map(char => `<span>${char}</span>`).join("")
-by.innerText = phrase.by
+phraseContainer.innerHTML = phrase.text
+                              .split("")
+                              .map(char => `<span>${char}</span>`)
+                              .join("");
+by.innerText = phrase.by;
 
-const chars = document.querySelectorAll(".phrase > span")
-chars[0].classList.add("active")
+const chars = document.querySelectorAll(".phrase > span");
+chars[0].classList.add("active");
 
-let start
-let index = 0
-let errors = 0
-let firstKeyPress = true
-let lastPressError = false
+let start;
+let index = 0;
+let errors = 0;
+let firstKeyPress = true;
+let lastPressError = false;
 
 document.body.addEventListener("keydown", handlerKeyDown)
 
 function handlerKeyDown(e) {
   if (index == phrase.text.length - 1) {
-    chars[index].classList.remove("active")
-    document.body.removeEventListener("keydown", handlerKeyDown)
-    showStatistics()
-    return
+    chars[index].classList.remove("active");
+    document.body.removeEventListener("keydown", handlerKeyDown);
+    showStatistics();
+    return;
   }
 
   if (e.key == chars[index].innerText || (e.key == "Enter" && chars[index].innerText == " ")) {
     if (firstKeyPress) {
-      start = Date.now()
-      firstKeyPress = false
+      start = Date.now();
+      firstKeyPress = false;
     }
-    chars[index].classList.remove("active")
-    index++
-    chars[index].classList.add("active")
+    chars[index].classList.remove("active");
+    index++;
+    chars[index].classList.add("active");
     if (lastPressError) {
-      lastPressError = false
-      chars[index-1].classList.add("error")
-      errors++
+      lastPressError = false;
+      chars[index-1].classList.add("error");
+      errors++;
     }
   } else if (!e.shiftKey) { // ignore shift key
-    lastPressError = true
+    lastPressError = true;
   }
 }
 
 function showStatistics() {
-  const words = phrase.text.split(" ").length
-  const minutes = (Date.now() - start) / 1000 / 60
-  const speed = Math.round(words / minutes)
+  const words = phrase.text.split(" ").length;
+  const minutes = (Date.now() - start) / 1000 / 60;
+  const speed = Math.round(words / minutes);
 
-  const statistics = document.querySelector(".statistics")
-  const precision = Math.round((phrase.text.length - errors) / phrase.text.length * 100)
+  const statistics = document.querySelector(".statistics");
+  const precision = Math.round((phrase.text.length - errors) / phrase.text.length * 100);
 
   statistics.innerHTML = `
     <div>
@@ -59,5 +62,5 @@ function showStatistics() {
       <p>Errors: ${errors}</p>
       <button onclick="location.reload()" class="next-phrase">ANOTHER PHRASE</button>
     </div>
-  `
+  `;
 }
