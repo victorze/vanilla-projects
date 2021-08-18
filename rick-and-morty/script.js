cbb420ae3373c81e35ea6e91759abab284d2791d
Victor <victorze@protonmail.com>
2021-08-18 17:08:00 -0500

    [Rick and Morty] Add style and script

--- rick-and-morty/script.js
+++ rick-and-morty/script.js
@@ -1,11 +1,85 @@
-(async () => {
-  const characters = await getCharacters(1)
-  console.log(characters.results);
-  characters.results.forEach(result => console.log(result.name));
-  showCharacters(characters.results);
-})();
+const cardsNode = document.querySelector(".cards");
+const loaderNode = document.querySelector(".loader");
 
-const cardsNode = document.querySelector(".container");
+let currentPage = 1;
+const limit = 20;
+let total = 0;
+
+loadCharacters(currentPage, limit);
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5 &&
+    hasMoreCharacters(currentPage, limit, total)) {
+    currentPage++;
+    loadCharacters(currentPage, limit);
+  }
+}, {
+  passive: true
+});
+
+async function loadCharacters(page, limit) {
+  showLoader();
+
+  setTimeout(async () => {
+    try {
+      if (hasMoreCharacters(page, limit, total)) {
+        const response = await getCharacters(page);
+        showCharacters(response.results);
+        total = response.info.count;
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      hideLoader();
+    }
+  }, 500);
+}
+
+function showCharacters(characters) {
+  let fragment = document.createDocumentFragment();
+
+  characters.forEach((character) =>  {
+    const cardNode = document.createElement("article");
+    cardNode.classList.add("card");
+
+    cardNode.innerHTML = `
+      <div class="card-image">
+        <img src="${character.image}" alt="${character.name}">
+      </div>
+      <div class="card-content">
+        <div class="section">
+          <h2 class="name">${character.name}</h2>
+          <span class="status">
+            <div class="status-icon ${character.status.toLowerCase()}"></div>
+              ${character.status} - ${character.species}
+          </span>
+        </div>
+        <div class="section">
+          <span class="text-gray">
+            Last known location:
+          </span>
+          <span class="location">
+            ${character.location.name}
+          </span>
+        </div>
+        <div class="section">
+          <span class="text-gray">
+            First seen in:
+          </span>
+          <span class="origin">
+            ${character.origin.name}
+          </span>
+        </div>
+      </div>
+    `;
+
+    fragment.appendChild(cardNode);
+  });
+
+  cardsNode.appendChild(fragment);
+}
 
 async function getCharacters(page) {
   const API_URL = `https://rickandmortyapi.com/api/character?page=${page}`;
@@ -18,35 +92,15 @@ async function getCharacters(page) {
   return await response.json();
 }
 
-function showCharacters(characters) {
-  let fragment = document.createDocumentFragment();
+function hideLoader() {
+  loaderNode.classList.remove("show");
+}
 
-  characters.forEach((character) =>  {
-    const cardNode = document.createElement("section");
-    cardNode.classList.add("card");
+function showLoader() {
+  loaderNode.classList.add("show");
+}
 
-    cardNode.innerHTML = `
-      <div class="content">
-        <div class="front">
-          <img src="${character.image}" alt="">
-        </div>
-        <div class="back">
-          <span class="name">${character.name}</span>
-          <span class="status">
-            <span class="status-icon ${character.status.toLowerCase()}">
-            </span>
-            ${character.status} - ${character.species}
-          </span>
-          <span class="text-gray">Last known location:</span>
-          <span class="location">${character.location.name}</span>
-          <span class="text-gray">First seen in:</span>
-          <span class="origin">${character.origin.name}</span>
-        </div>
-      </div>
-    `;
-
-    fragment.appendChild(cardNode);
-  });
-
-  cardsNode.appendChild(fragment);
+function hasMoreCharacters(page, limit, total) {
+  const startIndex = (page - 1) * limit + 1;
+  return total === 0 || startIndex < total;
 }
